cb8eeeaf4be40cfd934792692426341134f8bade
omuletzu <mihneasimota1234@gmail.com>
2025-05-10 21:41:05 +0300

    fixed async

--- src/main/java/com/siemens/internship/service/ItemService.java
+++ src/main/java/com/siemens/internship/service/ItemService.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -19,16 +16,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ItemService {
     @Autowired
     private ItemRepository itemRepository;
-    private static final ExecutorService executor = Executors.newFixedThreadPool(10);
-
-    // aceasta lista contine toate elementele finale procesate
-    private List<Item> itemsProcessed = new ArrayList<>();
-
-    // counter atomic pentru a numara cate iteme au fost procesate cu succes
-    private AtomicInteger processedCount = new AtomicInteger(0);
-
-    // asemanator cu cel de sus doar ca pentru cele fara de succes
-    private AtomicInteger failedCount = new AtomicInteger(0);
 
     // logger folosit pentru a afisa mesaje in timpul executiei
     private final Logger LOGGER = LoggerFactory.getLogger(ItemService.class);
@@ -74,70 +61,87 @@ public class ItemService {
      * Aceasta este o metoda asincrona deoarece in ea itemele sunt procesate in paralel
      * Se foloseste cate un thread pentru fiecare item id folosind CompletableFuture
      */
-    @Async  // adnotare folosita pentru face metoda sa fie executata asincron
+
+    @Async("taskExecutor")
+    public CompletableFuture<Item> processSingleItem(Long id,
+                                                     AtomicInteger processedCount,
+                                                     AtomicInteger failedCount) {
+        try {
+            // se obtine item-ul din baza de data dupa id
+            Optional<Item> optionalItem = itemRepository.findById(id);
+
+            //daca este gasit se proceseaza
+            if (optionalItem.isPresent()) {
+                Item item = optionalItem.get();
+
+                // item-ul se proceseaza si se salveaza inapoi in baza de date
+                item.setStatus("PROCESSED");
+                itemRepository.save(item);
+
+                // se incrementeaza atomic numarul de elemente procesate
+                processedCount.incrementAndGet();
+
+                return CompletableFuture.completedFuture(item);
+            }
+            //daca item-ul nu este gasit
+            else {
+                // se incrementeaza numarul de elemente ce nu au putut fi procesate
+                failedCount.incrementAndGet();
+
+                // se afiseaza un mesaj corespunzator
+                LOGGER.warn("Item not found with ID " + id);
+            }
+        }
+        catch (Exception e) {
+            // elementul nu a putut fi procesat deci creste numarul elementelor neprocesate si se afiseaza un mesaj corespunzator
+            failedCount.incrementAndGet();
+            LOGGER.error("Error while processing item ID " + id);
+        }
+
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @Async("taskExecutor")
     public CompletableFuture<List<Item>> processItemsAsync() {
 
         // in itemIds vor fi toate id-urile item-urilor din baza de date
         List<Long> itemIds = itemRepository.findAllIds();
 
         // aceasta lista contine toate task-urile (cate unul pentru fiecare id), pentru care se va astepta terminarea executiei
-        List<CompletableFuture<Void>> futureThreadsList = new ArrayList<>();
+        List<CompletableFuture<Item>> futureThreadsList = new ArrayList<>();
 
-        // aceasta variabila este atomica deoarece fiecare thread in parte o poate accesa si modifica
-        processedCount = new AtomicInteger(0);
+        // counter atomic pentru a numara cate iteme au fost procesate cu succes (atomic pentru ca poate fi accesat de mai multe thread-uri)
+        AtomicInteger processedCount = new AtomicInteger(0);
 
-        failedCount = new AtomicInteger(0);
-
-        // aici se foloseste synchronized pentru a face lista thread-safe, deoarece cu aceasta vor lucra mai multe thread-uri
-        itemsProcessed = Collections.synchronizedList(new ArrayList<>());
+        // asemanator cu cel de sus doar ca pentru cele fara de succes
+        AtomicInteger failedCount = new AtomicInteger(0);
 
         // se trece prin fiecare id
         for (Long id : itemIds) {
-
-            // acesta reprezinta un task asincron ce proceseaza item-ul (este cate unul pentru fiecare id)
-            CompletableFuture<Void> futureThread = CompletableFuture.runAsync(() -> {
-               try {
-
-                   // se obtine item-ul din baza de data dupa id
-                   Optional<Item> optionalItem = itemRepository.findById(id);
-
-                   //daca este gasit se proceseaza
-                   if (optionalItem.isPresent()) {
-                       Item item = optionalItem.get();
-
-                       // item-ul se proceseaza si se salveaza inapoi in baza de date
-                       item.setStatus("PROCESSED");
-                       itemRepository.save(item);
-
-                       // se incrementeaza atomic numarul de elemente procesate si se adauga in lista de iteme procesate
-                       itemsProcessed.add(item);
-                       processedCount.incrementAndGet();
-                   }
-                   //daca item-ul nu este gasit
-                   else {
-
-                       // se incrementeaza numarul de elemente ce nu au putut fi procesate
-                       failedCount.incrementAndGet();
-
-                       // se afiseaza un mesaj corespunzator
-                       LOGGER.warn("Item not found with ID " + id);
-                   }
-               }
-               catch (Exception e) {
-                    // elementul nu a putut fi procesat deci creste numarul elementelor neprocesate si se afiseaza un mesaj corespunzator
-                    failedCount.incrementAndGet();
-                    LOGGER.error("Error while processing item ID " + id);
-               }
-            }, executor);
-
             // task-ul curent este adaugat in lista cu toate task-urile
-            futureThreadsList.add(futureThread);
+            futureThreadsList.add(processSingleItem(id, processedCount, failedCount));
         }
 
         return CompletableFuture
                 .allOf(futureThreadsList.toArray(new CompletableFuture[0])) // asa se asteapta terminarea tuturor task-urilor din lista
                 .thenApply(x -> {   // dupa ce toate sunt gata se afiseaza cate au procesate in total, cate cu succes si cate nu
-                    LOGGER.info("Processed: " + itemsProcessed.size() + " items, successfully: " + processedCount.get() + " , failed : " + failedCount.get());
+                    LOGGER.info("Processed successfully: " + processedCount.get() + " , failed : " + failedCount.get());
+
+                    List<Item> itemsProcessed = new ArrayList<>();
+
+                    // dupa incheierea tuturor task-urilor se incearca extragerea item-ului
+                    // chiar daca nu s-a reusit extragerea item-ul tot a fost procesat, de aceea contoarele de processed si failed se modifica in cealalta functie
+                    for(CompletableFuture<Item> futureThread : futureThreadsList) {
+                        try {
+                            Item item = futureThread.join();
+                            if(item != null) {
+                                itemsProcessed.add(item);
+                            }
+                        }
+                        catch (Exception e) {
+                            LOGGER.error("Error retrieving processed item from task");
+                        }
+                    }
 
                     // aici se returneaza lista de elemente procesate
                     return itemsProcessed;
